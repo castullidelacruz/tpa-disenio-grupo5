ed42439d5541237b33283ddd5dbc4bc882e1e1ab
cuso03 <dasilva@frba.utn.edu.ar>
2025-07-04 18:13:44 -0300

    falta main

--- src/main/java/ar/edu/utn/frba/dds/dominio/Main.java
+++ src/main/java/ar/edu/utn/frba/dds/dominio/Main.java
@@ -1,5 +1,10 @@
 package ar.edu.utn.frba.dds.dominio;
+
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 /*<<<<<<< NavegacionModos
 import java.time.*;
 import java.util.List;
@@ -54,7 +59,8 @@ public class Main {
 
             long delayInicial = Duration.between(ahora, proximaEjecucion).toMillis();
 
-            // Programar la tarea para que se ejecute cada 24 horas a partir de la próxima ejecución a las 2 AM
+            // Programar la tarea para que se ejecute cada 24 horas
+            // a partir de la próxima ejecución a las 2 AM
             scheduler.scheduleAtFixedRate(
                 tareaConsenso,
                 delayInicial,
@@ -69,46 +75,42 @@ public class Main {
     }
 }
 */
-
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.time.LocalDate;
-
-
 //No esta terminado todavia
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("INICIO: Ejecución iniciada a las " + LocalDateTime.now());
-        try {
-          Hecho hecho = new Hecho("Corte de luz Dinamica",
-        "Corte de luz en zona sur","cortes",21.2,
-        12.8, LocalDate.of(2025,1,1),
-        LocalDate.now(),TipoFuente.DINAMICA,"",Boolean.TRUE);
-          
-
-          FuenteDataSet fuenteDataSet = new FuenteDataSet("EjHechos.csv","d/M/yyyy",';');
-          RepositorioHechos repositorioDeHechos = new RepositorioHechos();
-          RepositorioHechos repositorioDeHechos2 = new RepositorioHechos();
-          repositorioDeHechos2.cargarHecho(hecho);
-          FuenteDinamica fuenteDinamica = new FuenteDinamica(repositorioDeHechos2);
-          RepositorioFuentes repositorioDeFuentes = new RepositorioFuentes();
-          ConexionMock conexion = new ConexionMock();
-          URL url = new URL("http://mock.url");
-          FuenteProxyDemo fuenteProxy = new FuenteProxyDemo(conexion, url, repositorioDeHechos);
-          fuenteProxy.obtenerHechos();
-          repositorioDeFuentes.registrarFuente(fuenteDinamica);
-          repositorioDeFuentes.registrarFuente(fuenteDataSet);
-          repositorioDeFuentes.registrarFuente(fuenteProxy);
-          FiltroBaseAgregador filtroBase = new FiltroBaseAgregador();
-          Agregador agregador = new Agregador(repositorioDeFuentes, filtroBase);
-          agregador.agregarHechos();
-          List<Hecho> hechos = agregador.getHechos();
-          System.out.println("Cantidad de hechos: " + hechos.size());
+  public static void main(String[] args) {
+    System.out.println("INICIO: Ejecución iniciada a las " + LocalDateTime.now());
+    try {
+      Hecho hecho = new Hecho("Corte de luz Dinamica",
+          "Corte de luz en zona sur", "cortes", 21.2,
+          12.8, LocalDate.of(2025, 1, 1),
+          LocalDate.now(), TipoFuente.DINAMICA, "", Boolean.TRUE);
 
 
-        } catch (Exception e) {
-            System.err.println("ERROR: " + e.getMessage());
-        } 
-            System.out.println("FIN: Ejecución finalizada a las " + LocalDateTime.now());
-    
+      RepositorioHechos repositorioDeHechos = new RepositorioHechos();
+      RepositorioHechos repositorioDeHechos2 = new RepositorioHechos();
+      repositorioDeHechos2.cargarHecho(hecho);
+      FuenteDinamica fuenteDinamica = new FuenteDinamica(repositorioDeHechos2);
+      RepositorioFuentes repositorioDeFuentes = new RepositorioFuentes();
+      ConexionMock conexion = new ConexionMock();
+      URL url = new URL("http://mock.url");
+      FuenteProxyDemo fuenteProxy = new FuenteProxyDemo(conexion, url, repositorioDeHechos);
+      fuenteProxy.obtenerHechos();
+      FuenteDataSet fuenteDataSet = new FuenteDataSet("EjHechos.csv", "d/M/yyyy",
+          ';');
+      repositorioDeFuentes.registrarFuente(fuenteDinamica);
+      repositorioDeFuentes.registrarFuente(fuenteDataSet);
+      repositorioDeFuentes.registrarFuente(fuenteProxy);
+      FiltroBaseAgregador filtroBase = new FiltroBaseAgregador();
+      Agregador agregador = new Agregador(repositorioDeFuentes, filtroBase);
+      agregador.agregarHechos();
+      List<Hecho> hechos = agregador.getHechos();
+      System.out.println("Cantidad de hechos: " + hechos.size());
+
+    } catch (Exception e) {
+      System.err.println("ERROR: " + e.getMessage());
+    }
+    System.out.println("FIN: Ejecución finalizada a las " + LocalDateTime.now());
+
+  }
+}
