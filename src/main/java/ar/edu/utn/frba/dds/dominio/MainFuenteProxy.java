2170ff33fff1ebd7f21dc5a6edd2a1c0de08affa
kflores12 <kfloresportillo@frba.utn.edu.ar>
2025-07-04 15:37:01 -0300

    Planificacion externa del agregador

---
+++ src/main/java/ar/edu/utn/frba/dds/dominio/MainFuenteProxy.java
@@ -0,0 +1,26 @@
+package ar.edu.utn.frba.dds.dominio;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+
+
+public class MainFuenteProxy {
+    public static void main(String[] args) {
+        System.out.println("INICIO: Ejecución iniciada a las " + LocalDateTime.now());
+        try {
+            ConexionMock conexion = new ConexionMock();
+            URL url = new URL("http://mock.url");
+            RepositorioHechos repo = new RepositorioHechos();
+
+            FuenteProxyDemo fuente = new FuenteProxyDemo(conexion, url, repo);
+            fuente.obtenerHechos();
+
+            System.out.println("EXITO: Hechos procesados: " + repo.obtenerTodos().size());
+        } catch (Exception e) {
+            System.err.println("ERROR: " + e.getMessage());
+        } 
+            System.out.println("FIN: Ejecución finalizada a las " + LocalDateTime.now());
+    }
+}
+
+
